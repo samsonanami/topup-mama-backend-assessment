87addd57e4953a534104714381bcbf22d4d7f801
samson anami <samsonanami@gmail.com>
2022-03-28 20:54:26 +0300

    Add character documenttations

--- api/app/Http/Controllers/BookController.php
+++ api/app/Http/Controllers/BookController.php
@@ -77,6 +77,7 @@ class BookController extends Controller
         return $this->successResponse(BookResource::collection($books), 'Books found!', 200);
     }
     /**
+     * 
      * Create Books
      *
      * This endpoint allows you to add a new book to the list. 
@@ -86,9 +87,31 @@ class BookController extends Controller
      * @bodyParam authors array List of authors of the book. Example: ["George R. R. Martin"]
      * @bodyParam numberOfPages number Number of pages in the book. Example: 694
      * @bodyParam publisher string The book's publisher. Example: Bantam Books
-     * @bodyParam publisher string The book's publisher. Example: Bantam Books
      * 
-     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Book created!",
+     *    "data": {
+     *        "id": 7,
+     *        "name": "A Game of Thrones",
+     *        "isbn": "978-0553103540",
+     *        "authors": "[\"George R. R. Martin\"]",
+     *        "character_id": "1",
+     *        "pov_character_id": "1",
+     *        "numberOfPages": "694",
+     *        "publisher": "Bantam Books",
+     *        "country": "United States",
+     *        "mediaType": "Hardcover",
+     *        "released": "1996-08-01T00:00:00",
+     *        "comment_count": 0,
+     *        "povCharacters": [],
+     *        "characters": [],
+     *        "comments": [],
+     *        "created_at": "2022-03-28T16:47:12.000000Z",
+     *        "updated_at": "2022-03-28T16:47:12.000000Z"
+     *    }
+     *}
      */
     public function store(Request $request)
     {
@@ -104,23 +127,178 @@ class BookController extends Controller
             'pov_character_id' => 'integer|required|exists:characters,id'
         ]);
         $book = Book::create($request->all());
-        return response()->json(new BookResource($book));
+        return $this->successResponse(new BookResource($book), 'Book created!', 200);
     }
+    /**
+     * 
+     * Show Books
+     *
+     * This endpoint show a book by ID. 
+     * 
+     * @queryParam id integer required The unique ID of a book. Example: 2
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Book found!",
+     *    "data": {
+     *        "id": 2,
+     *        "name": "A Game of Thrones",
+     *        "isbn": "978-0553103540",
+     *        "authors": "[\"George R. R. Martin\"]",
+     *        "character_id": 1,
+     *        "pov_character_id": 1,
+     *        "numberOfPages": 694,
+     *        "publisher": "Bantam Books",
+     *        "country": "United States",
+     *        "mediaType": "Hardcover",
+     *        "released": "1996-08-01 00:00:00",
+     *        "comment_count": 2,
+     *        "povCharacters": [],
+     *        "characters": [],
+     *        "comments": [
+     *            {
+     *                "id": 3,
+     *                "book_id": 2,
+     *                "comment": "Test comment",
+     *                "commenter_ip": "192.168.1.112",
+     *                "created_at": "2022-03-28T11:27:13.000000Z",
+     *                "updated_at": "2022-03-28T11:27:13.000000Z"
+     *            },
+     *            {
+     *                "id": 4,
+     *                "book_id": 2,
+     *                "comment": "Test comment one",
+     *                "commenter_ip": "192.168.1.112",
+     *                "created_at": "2022-03-28T11:32:14.000000Z",
+     *                "updated_at": "2022-03-28T11:32:14.000000Z"
+     *            }
+     *        ],
+     *        "created_at": "2022-03-27T19:41:09.000000Z",
+     *        "updated_at": "2022-03-27T19:41:09.000000Z"
+     *    }
+     *}
+     */
     public function show($id)
     {
         $book = Book::find($id);
-        return response()->json(new BookResource($book));
+        return $this->successResponse(new BookResource($book), 'Book found!', 200);
     }
+    /**
+     * 
+     * Update Book
+     *
+     * This endpoint updates a book by ID. 
+     * 
+     * @queryParam id integer required The unique ID of a book. Example: 2
+     * @bodyParam name string required The name of the book. Example: A Game of Thrones
+     * @bodyParam isbn string The isbn number of the book. Example: 978-0553103540
+     * @bodyParam authors array List of authors of the book. Example: ["George R. R. Martin"]
+     * @bodyParam numberOfPages number Number of pages in the book. Example: 694
+     * @bodyParam publisher string The book's publisher. Example: Bantam Books
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Book updated!",
+     *    "data": {
+     *        "id": 2,
+     *        "name": "A Game of Thrones",
+     *        "isbn": "978-0553103540",
+     *        "authors": "[\"George R. R. Martin\"]",
+     *        "character_id": "1",
+     *        "pov_character_id": "1",
+     *        "numberOfPages": "694",
+     *        "publisher": "Bantam Books",
+     *        "country": "United States",
+     *        "mediaType": "Hardcover",
+     *        "released": "1996-08-01T00:00:00",
+     *        "comment_count": 2,
+     *        "povCharacters": [],
+     *        "characters": [],
+     *        "comments": [
+     *            {
+     *                "id": 3,
+     *                "book_id": 2,
+     *                "comment": "Test comment",
+     *                "commenter_ip": "192.168.1.112",
+     *                "created_at": "2022-03-28T11:27:13.000000Z",
+     *                "updated_at": "2022-03-28T11:27:13.000000Z"
+     *            },
+     *            {
+     *                "id": 4,
+     *                "book_id": 2,
+     *                "comment": "Test comment one",
+     *                "commenter_ip": "192.168.1.112",
+     *                "created_at": "2022-03-28T11:32:14.000000Z",
+     *                "updated_at": "2022-03-28T11:32:14.000000Z"
+     *            }
+     *        ],
+     *        "created_at": "2022-03-27T19:41:09.000000Z",
+     *        "updated_at": "2022-03-28T16:55:37.000000Z"
+     *    }
+     *}
+     */
     public function update(Request $request, $id)
     {
         $book = Book::find($id);
         $book->update($request->all());
-        return response()->json(new BookResource($book));
+        return $this->successResponse(new BookResource($book), 'Book updated!', 200);
     }
+    /**
+     * 
+     * Delete Book
+     *
+     * This endpoint delete a book by ID. 
+     * 
+     * @queryParam id integer required The unique ID of a book. Example: 2
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Book deleted!",
+     *    "data": {
+     *        "id": 2,
+     *        "name": "A Game of Thrones",
+     *        "isbn": "978-0553103540",
+     *        "authors": "[\"George R. R. Martin\"]",
+     *        "character_id": 1,
+     *        "pov_character_id": 1,
+     *        "numberOfPages": 694,
+     *        "publisher": "Bantam Books",
+     *        "country": "United States",
+     *        "mediaType": "Hardcover",
+     *        "released": "1996-08-01 00:00:00",
+     *        "comment_count": 2,
+     *        "povCharacters": [],
+     *        "characters": [],
+     *        "comments": [
+     *            {
+     *                "id": 3,
+     *                "book_id": 2,
+     *                "comment": "Test comment",
+     *                "commenter_ip": "192.168.1.112",
+     *                "created_at": "2022-03-28T11:27:13.000000Z",
+     *                "updated_at": "2022-03-28T11:27:13.000000Z"
+     *            },
+     *            {
+     *                "id": 4,
+     *                "book_id": 2,
+     *                "comment": "Test comment one",
+     *                "commenter_ip": "192.168.1.112",
+     *                "created_at": "2022-03-28T11:32:14.000000Z",
+     *                "updated_at": "2022-03-28T11:32:14.000000Z"
+     *            }
+     *        ],
+     *        "created_at": "2022-03-27T19:41:09.000000Z",
+     *        "updated_at": "2022-03-27T19:41:09.000000Z"
+     *    }
+     *}
+     */
     public function destroy($id)
     {
         $book = Book::find($id);
         $book->delete();
-        return response()->json('Item deleted successfully');
+        return $this->successResponse(new BookResource($book), 'Book deleted!', 200);
     }
 }
--- api/app/Http/Controllers/CharacterController.php
+++ api/app/Http/Controllers/CharacterController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Character;
 use Illuminate\Http\Request;
 use App\Http\Resources\CharacterResource;
+use App\Traits\ApiResponse;
 
 /**
  * 
@@ -14,20 +15,123 @@ use App\Http\Resources\CharacterResource;
  */
 class CharacterController extends Controller
 {
+    use ApiResponse;
     /**
      * 
      * List Characters
      * 
      * Display a listing of all the characters.
-     *
      * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Characters found!",
+     *    "data": [
+     *        {
+     *            "name": "Jon Snow",
+     *            "gender": "Male",
+     *            "culture": "Northmen",
+     *            "born": "In 283 AC",
+     *            "died": "\"\"",
+     *            "titles": "[\"Lord Commander of the Night's Watch\"]",
+     *            "aliases": "[\"Lord Snow\", \"Ned Stark's Bastard\", \"The Snow of Winterfell\", \"The Crow-Come-Over\", \"The 998th Lord Commander of the Night's Watch\", \"The Bastard of Winterfell\", \"The Black Bastard of the Wall\", \"Lord Crow\"]",
+     *            "father": "",
+     *            "mother": null,
+     *            "spouse": null,
+     *            "allegiances": null,
+     *            "books": null,
+     *            "povBooks": null,
+     *            "tvSeries": null,
+     *            "playedBy": null
+     *        }
+     *    ]
+     *}
      * 
      */
     public function index()
     {
         $characters = Character::all();
-        return response()->json($characters);
+        return $this->successResponse(CharacterResource::collection($characters), 'Characters found!', 200);
     }
+    /**
+     * 
+     * Show a character by ID
+     * 
+     * Display a single character by it's unique ID.
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Character found!",
+     *    "data": [
+     *        {
+     *            "name": "Jon Snow",
+     *            "gender": "Male",
+     *            "culture": "Northmen",
+     *            "born": "In 283 AC",
+     *            "died": "\"\"",
+     *            "titles": "[\"Lord Commander of the Night's Watch\"]",
+     *            "aliases": "[\"Lord Snow\", \"Ned Stark's Bastard\", \"The Snow of Winterfell\", \"The Crow-Come-Over\", \"The 998th Lord Commander of the Night's Watch\", \"The Bastard of Winterfell\", \"The Black Bastard of the Wall\", \"Lord Crow\"]",
+     *            "father": "",
+     *            "mother": null,
+     *            "spouse": null,
+     *            "allegiances": null,
+     *            "books": null,
+     *            "povBooks": null,
+     *            "tvSeries": null,
+     *            "playedBy": null
+     *        }
+     *    ]
+     *}
+     * 
+     */
+    public function show($id)
+    {
+        $character = Character::find($id);
+        return $this->successResponse(CharacterResource::collection($characters), 'Characters found!', 200);
+    }
+    /**
+     * 
+     * Update Character
+     * 
+     * This endpoint creates a character
+     * 
+     * @bodyParam name string required The name of the character. Example: Jon Snow
+     * @bodyParam gender string The gender  of the character. Example: Male
+     * @bodyParam culture string The culture of the character. Example: Northmen
+     * @bodyParam born date The date of birth of the character. Example: In 283 AC
+     * @bodyParam titles array The titles of the character. Example: ["Lord Commander of the Night's Watch"]
+     * @bodyParam aliases array The aliases of the character. Example: ["Lord Snow", "Ned Stark's Bastard"]
+     * @bodyParam father string The father of the character.
+     * @bodyParam mother string The mother of the character.
+     * @bodyParam spouse string The spouse of the character.
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Character updated!",
+     *    "data": [
+     *        {
+     *            "name": "Jon Snow",
+     *            "gender": "Male",
+     *            "culture": "Northmen",
+     *            "born": "In 283 AC",
+     *            "died": "\"\"",
+     *            "titles": "[\"Lord Commander of the Night's Watch\"]",
+     *            "aliases": "[\"Lord Snow\", \"Ned Stark's Bastard\", \"The Snow of Winterfell\", \"The Crow-Come-Over\", \"The 998th Lord Commander of the Night's Watch\", \"The Bastard of Winterfell\", \"The Black Bastard of the Wall\", \"Lord Crow\"]",
+     *            "father": "",
+     *            "mother": null,
+     *            "spouse": null,
+     *            "allegiances": null,
+     *            "books": null,
+     *            "povBooks": null,
+     *            "tvSeries": null,
+     *            "playedBy": null
+     *        }
+     *    ]
+     *}
+     * 
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -48,23 +152,95 @@ class CharacterController extends Controller
             'father' => 'string',
         ]);
         $character = Character::create($request->all());
-        return response()->json(new CharacterResource($character));
+        return $this->successResponse(CharacterResource::collection($character), 'Character created!', 200);
     }
-    public function show($id)
-    {
-        $character = Character::find($id);
-        return response()->json(new CharacterResource($character));
-    }
+    /**
+     * 
+     * Update Character
+     * 
+     * This endpoint allows you to update a character
+     * 
+     * @queryParam ID integer required The unique identifies for the character. Example: 3
+     * @bodyParam name string required The name of the character. Example: Jon Snow
+     * @bodyParam gender string The gender  of the character. Example: Male
+     * @bodyParam culture string The culture of the character. Example: Northmen
+     * @bodyParam born date The date of birth of the character. Example: In 283 AC
+     * @bodyParam titles array The titles of the character. Example: ["Lord Commander of the Night's Watch"]
+     * @bodyParam aliases array The aliases of the character. Example: ["Lord Snow", "Ned Stark's Bastard"]
+     * @bodyParam father string The father of the character.
+     * @bodyParam mother string The mother of the character.
+     * @bodyParam spouse string The spouse of the character.
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Character updated!",
+     *    "data": [
+     *        {
+     *            "name": "Jon Snow",
+     *            "gender": "Male",
+     *            "culture": "Northmen",
+     *            "born": "In 283 AC",
+     *            "died": "\"\"",
+     *            "titles": "[\"Lord Commander of the Night's Watch\"]",
+     *            "aliases": "[\"Lord Snow\", \"Ned Stark's Bastard\", \"The Snow of Winterfell\", \"The Crow-Come-Over\", \"The 998th Lord Commander of the Night's Watch\", \"The Bastard of Winterfell\", \"The Black Bastard of the Wall\", \"Lord Crow\"]",
+     *            "father": "",
+     *            "mother": null,
+     *            "spouse": null,
+     *            "allegiances": null,
+     *            "books": null,
+     *            "povBooks": null,
+     *            "tvSeries": null,
+     *            "playedBy": null
+     *        }
+     *    ]
+     *}
+     * 
+     */
     public function update(Request $request, $id)
     {
         $character = Character::find($id);
         $character->update($request->all());
-        return response()->json(new CharacterResource($character));
+        return $this->successResponse(CharacterResource::collection($character), 'Character updated!', 200);
     }
+    /**
+     * 
+     * Delete Character
+     * 
+     * This endpoint allows you to delete a character
+     * 
+     * @queryParam ID integer required The unique identifies for the character. Example: 3
+     * 
+     * @response
+     * {
+     *    "status": 200,
+     *    "message": "Character deleted!",
+     *    "data": [
+     *        {
+     *            "name": "Jon Snow",
+     *            "gender": "Male",
+     *            "culture": "Northmen",
+     *            "born": "In 283 AC",
+     *            "died": "\"\"",
+     *            "titles": "[\"Lord Commander of the Night's Watch\"]",
+     *            "aliases": "[\"Lord Snow\", \"Ned Stark's Bastard\", \"The Snow of Winterfell\", \"The Crow-Come-Over\", \"The 998th Lord Commander of the Night's Watch\", \"The Bastard of Winterfell\", \"The Black Bastard of the Wall\", \"Lord Crow\"]",
+     *            "father": "",
+     *            "mother": null,
+     *            "spouse": null,
+     *            "allegiances": null,
+     *            "books": null,
+     *            "povBooks": null,
+     *            "tvSeries": null,
+     *            "playedBy": null
+     *        }
+     *    ]
+     *}
+     * 
+     */
     public function destroy($id)
     {
         $character = Character::find($id);
         $character->delete();
-        return response()->json('Item deleted successfully');
+        return $this->successResponse(CharacterResource::collection($character), 'Character deleted!', 200);
     }
 }
